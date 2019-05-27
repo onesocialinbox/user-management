916c58a5f9dca92f6e6f51d2f20ae7ab909589c2
Ramya <rmramyait@gmail.com>
2019-05-27 12:16:01 +0530

    Contact File

--- views/auth/login.php
+++ views/auth/login.php
@@ -68,7 +68,7 @@ use yii\helpers\Url;
 
 							<li><?= Html::a('Rules', Url::to('@web/Rules.pdf'),['target'=>'_blank'])?></li>
 
-							<li> <?=Html::a('Contact Us', Url::to('@web/contact.txt'),['target'=>'_blank'])?></li>
+							<li> <?=Html::a('Contact Us', Url::to('@web/contacts.pdf'),['target'=>'_blank'])?></li>
 												
 						</ul>
 					</div>
